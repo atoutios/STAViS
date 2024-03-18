c0790a25b8047d48e7eb63085143223060972622
Earlitec <earlitec@EARLI23-02.local>
2024-03-18 14:44:10 -0700

    split

--- run_single_split_audiovisual_plus.sh
+++ run_single_split_audiovisual_plus.sh
@@ -11,7 +11,7 @@ mkdir -p ${base_path}'/'${results_path}
 python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 12 \
     --audiovisual --checkpoint 10 --n_epochs 60 \
     --root_path ${base_path} --result_path ${results_path} \
-    --resume_path ./data/pretrained_models/stavis_audiovisual/audiovisual_split${split}_save_60.pth \
+    --pretrain_path ./data/pretrained_models/stavis_audiovisual/audiovisual_split${split}_save_60.pth \
 	--annotation_path_coutrot1_train ./data/fold_lists/Coutrot_db1_list_train_${split}_fps.txt \
 	--annotation_path_coutrot1_test ./data/fold_lists/Coutrot_db1_list_test_${split}_fps.txt \
 	--annotation_path_coutrot2_train ./data/fold_lists/Coutrot_db2_list_train_${split}_fps.txt \
