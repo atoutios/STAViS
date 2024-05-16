c042650e6594b4542ec27e39581814502ce6aa49
Asterios Toutios <atoutios@earlitecdx.com>
2024-05-16 12:41:08 -0700

    Add audio path to script

--- run_all_splits_audiovisual_train_test.sh
+++ run_all_splits_audiovisual_train_test.sh
@@ -12,6 +12,7 @@ python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 12 \
     --audiovisual --checkpoint 10 --n_epochs 60 \
     --root_path ${base_path} --result_path ${results_path} \
     --resume_path ./data/earlitec_models/earlitec_split${split}_save_60.pth \
+	--audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
 	--pretrain_path ./data/pretrained_models/stavis_audiovisual/audiovisual_split1_save_60.pth \
 	--annotation_path_feasibility_train ./data/fold_lists/feasibility_list_train_${split}_fps.txt \
 	--annotation_path_feasibility_test ./data/fold_lists/feasibility_list_test_${split}_fps.txt
