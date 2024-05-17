ca5a9c629b411e4a0b58297801ccd8fd0fca298d
Asterios Toutios <atoutios@earlitecdx.com>
2024-05-17 09:20:30 -0700

    Editing script for new experiment

--- run_all_splits_audiovisual_train_test.sh
+++ run_all_splits_audiovisual_train_test.sh
@@ -9,9 +9,9 @@ results_path='split'${split}'_results'
 mkdir -p ${base_path}'/'${results_path}
 
 python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 12 \
-    --audiovisual --checkpoint 10 --n_epochs 60 \
+    --audiovisual --checkpoint 60 --n_epochs 300 \
     --root_path ${base_path} --result_path ${results_path} \
-    --resume_path ./data/earlitec_models/earlitec_split${split}_save_60.pth \
+    --resume_path ./data/earlitec_models/earlitec_split${split}_save_300.pth \
 	--audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
 	--pretrain_path ./data/pretrained_models/stavis_audiovisual/audiovisual_split1_save_60.pth \
 	--annotation_path_feasibility_train ./data/fold_lists/feasibility_list_train_${split}_fps.txt \
