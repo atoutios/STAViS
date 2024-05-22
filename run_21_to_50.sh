ab5499a35b5b11d2ca508bf61aa01efbbc13081c
Asterios Toutios <atoutios@earlitecdx.com>
2024-05-22 10:19:30 -0700

    Continuing experiment to more epochs

--- run_21_to_50.sh
+++ run_21_to_50.sh
@@ -12,6 +12,6 @@ python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 4 \
     --audiovisual --checkpoint 1 --n_epochs 30 \
     --root_path ${base_path} --result_path ${results_path} \
 	--audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
-	--pretrain_path ./experiments/asd20_step32/split1_results\save_20.pth \
+	--pretrain_path ./experiments/asd20_step32/split1_results/save_20.pth \
 	--annotation_path_feasibility_train ./data/fold_lists/feasibility_list_train_${split}_fps.txt \
 	--annotation_path_feasibility_test ./data/fold_lists/feasibility_list_test_${split}_fps.txt
