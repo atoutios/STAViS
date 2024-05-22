774044795ff6102b1511c07143211b8ad3a61726
Asterios Toutios <atoutios@earlitecdx.com>
2024-05-22 10:18:38 -0700

    Continuing experiment to more epochs

---
+++ run_21_to_50.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Train and test STAViS audiovisual models for all splits
+
+base_path='./experiments/asd20_step32_21_to_50'
+
+split=1
+results_path='split'${split}'_results'
+mkdir -p ${base_path}'/'${results_path}
+
+python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 4 \
+    --audiovisual --checkpoint 1 --n_epochs 30 \
+    --root_path ${base_path} --result_path ${results_path} \
+	--audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
+	--pretrain_path ./experiments/asd20_step32/split1_results\save_20.pth \
+	--annotation_path_feasibility_train ./data/fold_lists/feasibility_list_train_${split}_fps.txt \
+	--annotation_path_feasibility_test ./data/fold_lists/feasibility_list_test_${split}_fps.txt
