c5f412fa146f67b8f8912a1d81d26e45413f02d9
Asterios Toutios <atoutios@earlitecdx.com>
2024-05-22 09:13:20 -0700

    Setting to step=32

--- run_all_splits_audiovisual_train_test.sh
+++ run_all_splits_audiovisual_train_test.sh
@@ -2,14 +2,14 @@
 
 # Train and test STAViS audiovisual models for all splits
 
-base_path='./experiments/td30_step_update'
+base_path='./experiments/asd20_step32'
 
 split=1
 results_path='split'${split}'_results'
 mkdir -p ${base_path}'/'${results_path}
 
 python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 4 \
-    --audiovisual --checkpoint 30 --n_epochs 30 \
+    --audiovisual --checkpoint 1 --n_epochs 20 \
     --root_path ${base_path} --result_path ${results_path} \
 	--audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
 	--pretrain_path ./data/pretrained_models/stavis_audiovisual/audiovisual_split1_save_60.pth \
