f42723def718a7cc68a6010da872e9fb8f71e88a
Asterios Toutios <atoutios@earlitecdx.com>
2024-05-23 10:00:24 -0700

    Testing optimal asd step32 model

---
+++ run_all_splits_audiovisual_only_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Train and test STAViS audiovisual models for all splits
+
+base_path='./experiments/asd_step32_optimal_test'
+
+split=1
+results_path='split'${split}'_results'
+mkdir -p ${base_path}'/'${results_path}
+
+python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 4 \
+	--audiovisual \
+	--no_train --no_val \
+    --root_path ${base_path} --result_path ${results_path} \
+	--audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
+	--pretrain_path ./data/earlitec_models/asd_step32_optimal.pth \
+	--annotation_path_feasibility_train ./data/fold_lists/feasibility_list_train_${split}_fps.txt \
+	--annotation_path_feasibility_test ./data/fold_lists/feasibility_list_test_${split}_fps.txt
