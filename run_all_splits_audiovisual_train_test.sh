0346c8af5f7679100b2dbf014e37bc232c0ce561
Asterios Toutios <atoutios@earlitecdx.com>
2024-05-23 13:02:46 -0700

    Comparing step=32 to step=90

--- run_all_splits_audiovisual_train_test.sh
+++ run_all_splits_audiovisual_train_test.sh
@@ -10,7 +10,7 @@ results_path='split'${split}'_results'
 mkdir -p ${base_path}'/'${results_path}
 
 python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 4 \
-    --audiovisual --checkpoint 1	 --n_epochs 20 \
+    --audiovisual --checkpoint 1 --n_epochs 50 \
     --root_path ${base_path} --result_path ${results_path} \
 	--audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
 	--pretrain_path ./data/pretrained_models/stavis_audiovisual/audiovisual_split1_save_60.pth \
