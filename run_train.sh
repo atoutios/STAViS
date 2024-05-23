c263ee174a6a104531f41a87d4e6448068403e2d
Asterios Toutios <atoutios@earlitecdx.com>
2024-05-23 15:47:18 -0700

    Comparing step=32 to step=90

--- run_train.sh
+++ run_train.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 rm -r ~/STAViS/data/annotations/feasibility
-cp -r ~/STAViS/data/annotations/ASD/feasibility ~/STAViS/data/annotations
+cp -r ~/STAViS/data/annotations/TD/feasibility ~/STAViS/data/annotations
 
-base_path='./experiments/asd45_step90'
+base_path='./experiments/td45_step90'
 
 split=1
 results_path='split'${split}'_results'
 mkdir -p ${base_path}'/'${results_path}
 
 python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 4 \
-	--audiovisual \
-	--no_train --no_val \
+    --audiovisual --checkpoint 1 --n_epochs 45 \
     --root_path ${base_path} --result_path ${results_path} \
 	--audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
-	--pretrain_path ${base_path}/${results_path}/optimal_model.pth \
+	--pretrain_path ./data/pretrained_models/stavis_audiovisual/audiovisual_split1_save_60.pth \
 	--annotation_path_feasibility_train ./data/fold_lists/feasibility_list_train_${split}_fps.txt \
 	--annotation_path_feasibility_test ./data/fold_lists/feasibility_list_test_${split}_fps.txt
