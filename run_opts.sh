00d0e2df1100e4c1e30735d36b6730b4ef10d2cc
Asterios Toutios <atoutios@earlitecdx.com>
2024-05-23 17:15:34 -0700

    Comparing step=32 to step=90

--- run_opts.sh
+++ run_opts.sh
@@ -1,6 +1,6 @@
 rm -r ~/STAViS/data/annotations/feasibility
 cp -r ~/STAViS/data/annotations/TD/feasibility ~/STAViS/data/annotations
 
-base_path='./experiments/td45_step90'
+base_path='./experiments/td45_step32'
 split=1
 movie='0075PEER'
