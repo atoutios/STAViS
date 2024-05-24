2e8fc31ca9cfc0f582fa98cc34f5b53bd1376f1b
Asterios Toutios <atoutios@earlitecdx.com>
2024-05-24 14:32:08 -0700

    Ready for experiments

--- run_test.sh
+++ run_test.sh
@@ -8,13 +8,13 @@ results_path=${dx}'_split'${split}
 mkdir -p ${base_path}'/'${results_path}
 
 python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 4 \
-	--audiovisual \
-	--no_train --no_val \
-    --root_path ${base_path} --result_path ${results_path} \
-	--audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
-	--pretrain_path ${base_path}/${results_path}/optimal_model.pth \
-	--annotation_path_feasibility_train ./data/fold_lists/feasibility_list_train_${split}_fps.txt \
-	--annotation_path_feasibility_test ./data/fold_lists/feasibility_list_test_${split}_fps.txt
+        --audiovisual \
+        --no_train --no_val \
+    	--root_path ${base_path} --result_path ${results_path} \
+        --audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
+        --pretrain_path ${base_path}/${results_path}/optimal_model.pth \
+        --annotation_path_feasibility_train ./data/fold_lists/feasibility_list_train_${split}_fps.txt \
+        --annotation_path_feasibility_test ./data/fold_lists/feasibility_list_test_${split}_fps.txt
 
 curr_dir=`pwd`
 cd ${base_path}/${results_path}/feasibility/${movie}
