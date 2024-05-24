33269ad65c61144c8ff1453325802c254b033880
Asterios Toutios <atoutios@earlitecdx.com>
2024-05-23 17:05:03 -0700

    Comparing step=32 to step=90

--- run_test.sh
+++ run_test.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 
-rm -r ~/STAViS/data/annotations/feasibility
-cp -r ~/STAViS/data/annotations/TD/feasibility ~/STAViS/data/annotations
+source run_opts.sh
 
-base_path='./experiments/td45_step90'
-
-split=1
 results_path='split'${split}'_results'
 mkdir -p ${base_path}'/'${results_path}
 
-python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 4 \
-	--audiovisual \
-	--no_train --no_val \
-    --root_path ${base_path} --result_path ${results_path} \
-	--audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
-	--pretrain_path ${base_path}/${results_path}/optimal_model.pth \
-	--annotation_path_feasibility_train ./data/fold_lists/feasibility_list_train_${split}_fps.txt \
-	--annotation_path_feasibility_test ./data/fold_lists/feasibility_list_test_${split}_fps.txt
+# python3 main.py --gpu_devices 0 --batch_size 32 --n_threads 4 \
+# 	--audiovisual \
+# 	--no_train --no_val \
+#     --root_path ${base_path} --result_path ${results_path} \
+# 	--audio_pretrain_path ./data/pretrained_models/soundnet8.pth \
+# 	--pretrain_path ${base_path}/${results_path}/optimal_model.pth \
+# 	--annotation_path_feasibility_train ./data/fold_lists/feasibility_list_train_${split}_fps.txt \
+# 	--annotation_path_feasibility_test ./data/fold_lists/feasibility_list_test_${split}_fps.txt
 
 curr_dir=`pwd`
 cd ${base_path}/${results_path}/feasibility/${movie}
+echo ${movie}
+pwd
 mkdir _resized
 find . -maxdepth 1 -iname "*.jpg" | xargs -L1 -I{} convert -resize 640x480! "{}" _resized/"{}"
 cd $curr_dir
